7676fadbfe795938775c15ad3e481d9ca244b5d2
Mwangi-joan <mwangijoan16@gmail.com>
2019-05-07 23:14:13 -0700

    hello

---
+++ Mwangi/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\Mwangi\Desktop\projects\Mwangi\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
---
+++ websites/views.py
@@ -0,0 +1,38 @@
+from django.shortcuts import render
+from .models import Member
+from django.views.generic import ListView, CreateView,UpdateView,DeleteView
+
+
+# Create your views here.
+
+class MemberListView(ListView):
+    model = Member
+    template_name = 'websites/index.html'
+    context_object_name = 'profiles'
+
+
+    def get_query(self):
+        query=self.request.GET.get('q')
+        if query:
+
+            return Member.objects.filter(name__icontains=query) | Member.objects.filter(slogan__icontains=query)
+        else:
+            return Member.objects.all()
+
+class MemberCreateView(CreateView):
+    model = Member
+    fields = '__all__'
+
+class MemberUpdateView(UpdateView):
+    model = Member
+    fields = '__all__'
+
+class MemberDeleteView(DeleteView):
+    model=Member
+    success_url = '/'
+
+
+
+
+
+
